2af049ec735f4d839f87fec14632c58b54f1e4ad
pop marley <hsynozguven@hotmail.com>
2023-07-15 18:45:01 +0300

    Guncelleme(Bloglar dbden getirildi)

---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccesLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class BlogManager : IBlogService
+	{
+		IBlogDAL _blogDAL;
+
+		public BlogManager(IBlogDAL blogDAL)
+		{
+			_blogDAL = blogDAL;
+		}
+
+		public void BlogAdd(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BlogDelete(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BlogUpdate(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Blog GetByID(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Blog> GetList()
+		{
+			return _blogDAL.GetListAll();
+		}
+	}
+}
--- Proje_Kampı/Controllers/BlogController.cs
+++ Proje_Kampı/Controllers/BlogController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Proje_Kampı.Controllers
 {
 	public class BlogController : Controller
 	{
+		BlogManager bm=new BlogManager(new EFBlogRepository());
 		public IActionResult Index()
 		{
-			return View();
+			var values =bm.GetList();
+			return View(values);
 		}
 	}
 }
