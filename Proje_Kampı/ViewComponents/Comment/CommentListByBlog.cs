24c5a1954a7c093dd4737637dab81e87c1c2add0
pop marley <hsynozguven@hotmail.com>
2023-08-24 14:05:53 +0300

    Guncelleme(Bloglara Ait Yorumların Listelenmesi)

---
+++ Proje_Kampı/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proje_Kampı.ViewComponents.Comment
+{
+	public class CommentListByBlog : ViewComponent
+	{
+		CommentManager cm =new CommentManager(new EFCommentRepository());
+		public IViewComponentResult Invoke()
+		{
+			var values = cm.GetList(4);
+			return View(values);
+		}
+	}
+}
