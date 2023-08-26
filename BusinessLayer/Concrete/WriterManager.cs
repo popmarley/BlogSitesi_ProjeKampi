43ecf819b4dcccbea56ef846fa1b37434d71805a
pop marley <hsynozguven@hotmail.com>
2023-08-27 00:33:48 +0300

    Guncelleme(Yazar Kaydı Ve Sweet Alert )

---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccesLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class WriterManager:IWriterService
+	{
+		IWriterDAL _writerDAL;
+
+		public WriterManager(IWriterDAL writerDAL)
+		{
+			_writerDAL = writerDAL;
+		}
+
+		public void WriterAdd(Writer writer)
+		{
+			_writerDAL.Insert(writer);
+		}
+	}
+}
---
+++ Proje_Kampı/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proje_Kampı.Controllers
+{
+	public class RegisterController : Controller
+	{
+		WriterManager wm=new WriterManager (new EFWriterRepository ());
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Writer p)
+		{
+			p.WriterStatus = true;
+			p.WriterAbout = "deneme test";
+			wm.WriterAdd(p);
+			 return RedirectToAction("Index","Blog");
+		}
+	}
+}
